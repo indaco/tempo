9cafd8b81222657ba9244906bd29cc04db61ea94
indaco <indaco@users.noreply.github.com>
2025-03-24 00:22:13 +0100

    chore: simplify version test by removing unnecessary package import

--- internal/version/version_test.go
+++ internal/version/version_test.go
@@ -1,16 +1,14 @@
-package version_test
+package version
 
 import (
 	"testing"
-
-	"github.com/indaco/tempo/internal/version"
 )
 
 // TestGetVersion checks if the GetVersion function correctly retrieves the embedded version.
 func TestGetVersion(t *testing.T) {
 	expectedVersion := "0.2.1"
 
-	got := version.GetVersion()
+	got := GetVersion()
 	if got != expectedVersion {
 		t.Errorf("GetVersion() = %q; want %q", got, expectedVersion)
 	}
