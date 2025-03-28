0ec8691f1242a12e2f32e1e799e167a7859ea0cc
indaco <indaco@users.noreply.github.com>
2025-03-28 08:39:42 +0100

    chore(release) v0.2.3

--- internal/version/version_test.go
+++ internal/version/version_test.go
@@ -6,7 +6,7 @@ import (
 
 // TestGetVersion checks if the GetVersion function correctly retrieves the embedded version.
 func TestGetVersion(t *testing.T) {
-	expectedVersion := "0.2.2"
+	expectedVersion := "0.2.3"
 
 	got := GetVersion()
 	if got != expectedVersion {
